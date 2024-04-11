d7eb5af2db99e0e2aab118559d23c4128233bd6c
1zuu <isurualagiyawanna9717@gmail.com>
2024-04-11 17:20:27 +0530

    sample mongo package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.1"
+__version__ = "0.0.4"
 REPO_NAME = "mlops-learning-00"
 PKG_NAME= "samplemongoconnect"
 AUTHOR_USER_NAME = "machinelearningzuu"
